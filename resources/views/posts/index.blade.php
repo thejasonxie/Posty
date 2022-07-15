7849406bc8c16888defd3f1d2561df46a0909c78
Jason Xie <thejasonxie@gmail.com>
2022-07-15 12:50:51 -0400

    Listing posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,7 @@
   <div class="flex justify-center">
     <div class="w-8/12 bg-white p-6 rounded-lg">
       
-      <form action="{{ route("posts") }}" method="post">
+      <form action="{{ route("posts") }}" method="post" class="mb-4">
         @csrf
         <div class="mb-4">
           <label for="body" class="sr-only">Body</label>
@@ -13,18 +13,31 @@
           placeholder="Post something!"></textarea>
 
           @error("body")
-            <div class="text-red-500 my-2 text-sm">
+            <div class="text-red-500 mt-2 text-sm">
               {{ $message }}
             </div>
           @enderror
 
           <div>
-            <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded 
+            <button type="submit" class="bg-blue-500 text-white px-4 py-2 my-2 rounded 
             font-medium">Post</button>
           </div>
           
         </div>
       </form>
+
+      @if ($posts->count())
+        @foreach ($posts as $post)
+            <div class="mb-4">
+              <a href="" class="font-bold">{{ $post->user->name }}</a> <span 
+              class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
+            
+              <p class="mb-2">{{ $post->body }}</p>
+            </div>
+        @endforeach
+      @else
+        <p>There are no posts</p>
+      @endif
 
     </div>
   </div>
