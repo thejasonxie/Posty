19759210af957c0cbb1eb697bb8f42e13eacf4b4
Jason Xie <thejasonxie@gmail.com>
2022-07-15 01:46:02 -0400

    Signing in after registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -33,6 +33,7 @@ class RegisterController extends Controller
         ]);
 
         // sign the user in
+        auth()->attempt($request->only("email", "password"));
 
         // redirect
         return redirect()->route("dashboard");
--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,9 @@ class DashboardController extends Controller
 {
     public function index() 
     {
+
+        dd(auth()->user());
+        
         return view("dashboard");
     }
 }
