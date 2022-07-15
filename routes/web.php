dcb3b7f20e495b52f580502e8c1b0b52293aef03
Jason Xie <thejasonxie@gmail.com>
2022-07-15 01:41:49 -0400

    Registering users

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get("/dashboard", [DashboardController::class, "index"])->name("dashboard");
+
+Route::get("/register", [RegisterController::class, "index"])->name("register");
+Route::post("/register", [RegisterController::class, "store"]);
 
 Route::get('/posts', function () {
     return view('posts.index');
