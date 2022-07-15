8cc612e9dd4d4a9f50d99af0b9dafddf827ca677
Jason Xie <thejasonxie@gmail.com>
2022-07-15 00:03:27 -0400

    Styling the layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,34 @@
   <title>Posty</title>
   @vite('resources/css/app.css')
 </head>
-<body>
+<body class="bg-gray-200">
+  <nav class="p-6 bg-white flex justify-between mb-6">
+    <ul class="flex items-center">
+      <li>
+        <a href="" class="p-3">Home</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Dashboard</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Post</a>
+      </li>
+    </ul>
+    <ul class="flex items-center">
+      <li>
+        <a href="" class="p-3">Jason Xie</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Login</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Register</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Logout</a>
+      </li>
+    </ul>
+  </nav>
   @yield("content")
 </body>
 </html>
