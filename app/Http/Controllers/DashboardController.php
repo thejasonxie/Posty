8e7751cc26ad562d1ce63ddcbbacce87f35fd4c7
Jason Xie <thejasonxie@gmail.com>
2022-07-15 09:21:00 -0400

    Authenticated state

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,10 +7,7 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index() 
-    {
-
-        dd(auth()->user());
-        
+    {   
         return view("dashboard");
     }
 }
